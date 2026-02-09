83d219e80f6a22c92050af71e87aef1f5088de52
nhhun2005 <nhhun2005@gmail.com>
2026-02-09 09:15:32 +0700

    style: ui for score-page

---
+++ score-page.js
@@ -0,0 +1,8 @@
+const fullname = document.getElementById("fullname");
+fullname.textContent = "Nguyễn Hà Huân";
+const studentid = document.getElementById("studentid");
+studentid.textContent = "B2303869";
+const scoreValue = document.getElementById("scoreValue");
+scoreValue.textContent = "3.69";
+const gradeValue = document.getElementById("gradeValue");
+gradeValue.textContent += " A";
